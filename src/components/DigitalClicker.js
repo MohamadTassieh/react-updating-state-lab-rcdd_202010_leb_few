9921dda8a40e1caee5fe6ceac2acedf753a3908b
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-02-14 13:44:35 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,19 +3,19 @@ import React from 'react';
 class DigitalClicker extends React.Component {
    constructor() {
     super();
-    
+
     this.state = {
      timesClicked:0
     };
   }
 
-    
+
   handleClick = () => {
   this.setState({
     timesClicked:this.state.timesClicked + 1
   })
   };
-    
+
   render() {
     return (
     <div>
